277bcdd113b3630d30bd04b2d625b45b9ce0a4f7
C0mRedKed <redkedrock@icloud.com>
2022-09-27 00:22:29 +0300

    HW4

---
+++ HW4 task 3/Program.cs
@@ -0,0 +1,42 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+const int NEGATIVE_VALUE = -1;
+const int POSITIVE_VALUE = 1;
+
+int Prompt(string message)
+{
+Console.Write(message);
+bool isDigit = int.TryParse(Console.ReadLine(), out int number);
+if (isDigit){
+return number;
+}
+throw new Exception("Вы ввели не число");
+}
+
+int[] GenerateArray(int length, int minRandom, int maxRandom)
+{
+Random rnd = new Random();
+int[] answer = new int[length];
+for (int i = 0; i < answer.Length; i++)
+{
+answer[i] = rnd.Next(minRandom, maxRandom + 1);
+}
+return answer;
+}
+void PrintArray(int[] array)
+{
+for (int i = 0; i < array.Length; i++)
+{
+System.Console.Write(array[i]+ "\t");
+}
+System.Console.WriteLine();
+}
+
+const int MIN_ELEMENTS = -9;
+const int MAX_ELEMENTS = 9;
+
+int length = Prompt("Введите длину массива >");
+int[] array = GenerateArray(length, MIN_ELEMENTS, MAX_ELEMENTS);
+PrintArray(array);
---
+++ task 23/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int Prompt(string message)
+{
+System.Console.Write(message);
+return Convert.ToInt32(Console.ReadLine());
+}
+
+int CountDigit(int number)
+{
+    int count = 0;
+    if(number == 0)
+    {
+        return 1;
+    }
+    while (number != 0)
+    {
+        number = number / 10;
+        count++;
+    }
+    return count;
+}
+int number = Prompt("Введите число > ");
+System.Console.WriteLine(CountDigit(number));
