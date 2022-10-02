ce13d9fa4d5c8a4c2ca7e6bfa4fc9e1e4117ca02
C0mRedKed <redkedrock@icloud.com>
2022-10-02 11:55:23 +0300

    HW 6-7

---
+++ HW6 task 1/Program.cs
@@ -0,0 +1,66 @@
+﻿// // Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    bool count = int.TryParse(Console.ReadLine(), out int number);
+    if (count)
+    {
+        return number;
+    }
+    throw new Exception("Это не число!");
+}
+
+int[] InputArray(int len)
+{
+    int[] array = new int[len];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Prompt($"Введите {i + 1}-й элемент массива -> ");
+        // System.Console.Write($"Введите {i + 1}-й элемент массива -> ");
+        // array[i] = int.Parse(Console.ReadLine() ?? "0");
+        System.Console.WriteLine();
+    }
+    return array;
+}
+
+int PositiveNumber(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+void OutputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}\t");
+    }
+}
+
+void Chek(int count)
+{
+    if (count > 0)
+    {
+        int[] array = InputArray(count);
+        OutputArray(array);
+        System.Console.WriteLine();
+        System.Console.WriteLine($"Количество чисел больше 0 -> {PositiveNumber(array)}");
+    }
+    else
+    {
+        System.Console.WriteLine("Введено отрицательное число");
+    }
+}
+
+int count = Prompt("Введите размер масива -> ");
+Chek(count);
---
+++ HW6 task 2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    bool count = int.TryParse(Console.ReadLine(), out int number);
+    if (count)
+    {
+        return number;
+    }
+    throw new Exception("Это не число!");
+}
+(double, double) LinearFunction(double k1, double k2, double b1, double b2)
+{
+    double x = 0;
+    double y = 0;
+    x = (b2 - b1) / (k1 - k2);
+    y = (k1 * x) + b1;
+    return (x, y);
+}
+void Check(double k1, double k2, double b1, double b2)
+{
+    if (k1 == k2 && b1 != b2) System.Console.WriteLine("Линии паралельны");
+    else if (k1 == k2 && b1 == b2) System.Console.WriteLine("Линии совпадают");
+    else System.Console.WriteLine($"Линии пересекаются в точке {LinearFunction(k1, k2, b1, b2)}");
+}
+double b1 = Prompt("введите b1 > ");
+double k1 = Prompt("введите k1 > ");
+double b2 = Prompt("введите b2 > ");
+double k2 = Prompt("введите k2 > ");
+Check(k1, k2, b1, b2);
